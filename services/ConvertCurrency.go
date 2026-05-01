bce0da21a6f1be46246a1b016a3c477ccac0a8bc
Ignitz <moritz.pertl@gmx.at>
2026-05-01 14:04:08 +0200

    grcp

--- services/ConvertCurrency.go
+++ services/ConvertCurrency.go
@@ -3,13 +3,14 @@ package services
 import (
 	"SE-CarRentalService/services/proto"
 	"context"
-	"crypto/tls"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 type ConverterClient struct {
@@ -25,7 +26,6 @@ func NewConverterClientRetry() *ConverterClient {
 		}
 
 		fmt.Printf("Failed to connect to currency converter, retrying in 3 seconds: %v\n", err)
-
 		time.Sleep(3 * time.Second)
 	}
 }
@@ -33,15 +33,20 @@ func NewConverterClientRetry() *ConverterClient {
 func NewConverterClient() (*ConverterClient, error) {
 	host := os.Getenv("GRPCHOST")
 	port := os.Getenv("GRPCPORT")
-	target := host + ":" + port
 
-	creds := credentials.NewTLS(&tls.Config{
-		ServerName: host,
-	})
+	if host == "" {
+		return nil, fmt.Errorf("GRPCHOST is not set")
+	}
+
+	if port == "" {
+		return nil, fmt.Errorf("GRPCPORT is not set")
+	}
+
+	target := host + ":" + port
 
 	conn, err := grpc.NewClient(
 		target,
-		grpc.WithTransportCredentials(creds),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create gRPC client: %w", err)
@@ -60,6 +65,21 @@ func (c *ConverterClient) Close() error {
 func (c *ConverterClient) ConvertCurrency(initialCurrency string, initialAmount float64, targetCurrency string) (float64, string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
+	username := os.Getenv("BE_USER")
+	password := os.Getenv("BE_PASSWORD")
+
+	if username == "" {
+		return 0, "", fmt.Errorf("BE_USER is not set")
+	}
+
+	if password == "" {
+		return 0, "", fmt.Errorf("BE_PASSWORD is not set")
+	}
+
+	authValue := "Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", authValue)
 
 	reply, err := c.client.ConvertCurrency(ctx, &proto.ConvertCurrencyRequest{
 		InitialCurrency: initialCurrency,
